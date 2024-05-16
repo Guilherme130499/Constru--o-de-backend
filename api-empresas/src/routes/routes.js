ed0a03330263f92c274881aaa7ba0bcf61fc2cc3
Guilherme130499 <guilherme.s.correia@iesb.edu.br>
2024-05-15 21:37:11 -0300

    aula-api1505

--- api-empresas/src/routes/routes.js
+++ api-empresas/src/routes/routes.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
+//controllers
 const CargoController = require('../controllers/CargoController')
 
+//validators
+const {validarCargo} = require('../validators/CargoValidator')
 
 // cargo
-router.post('/cargos', CargoController.criar)
+router.post('/cargos', validarCargo, CargoController.criar)
 
 
 
