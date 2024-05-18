87986cf4d9f299572e47d47f343a3ce28ab02ee8
Guilherme130499 <guilherme.s.correia@iesb.edu.br>
2024-05-17 21:51:13 -0300

    atualização projeto

--- api-empresas/src/database/connection.js
+++ api-empresas/src/database/connection.js
@@ -7,9 +7,10 @@ const DB_HOST = process.env.DB_HOST
 const DB_NAME = process.env.DB_NAME
 
 function main() {
-    mongoose.connect(`mongodb+srv://guilhermeteste:gSNspHqdaS1bGQqx@servergui.t7g7vka.mongodb.net/?retryWrites=true&w=majority&appName=servergui`)
+    mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority&appName=servergui`)
     .then(() => console.log("Conectado ao MongoDB"))
     .catch(err => console.log("Erro ao conectar no MongoDB: ", err))
 }
 
 module.exports = main
+//${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}
--- api-empresas/src/routes/routes.js
+++ api-empresas/src/routes/routes.js
@@ -3,14 +3,18 @@ const router = express.Router()
 
 //controllers
 const CargoController = require('../controllers/CargoController')
+const DepartamentoController = require('../controllers/DepartamentoController')
 
 //validators
 const {validarCargo} = require('../validators/CargoValidator')
+const {validarDepartamento} = require('../validators/DepartamentoValidator')
 
 // cargo
+router.get('/cargos', CargoController.buscarTodos)
+router.get('/cargos/:id', CargoController.buscarTodos)
 router.post('/cargos', validarCargo, CargoController.criar)
-
-
+router.put('/cargos/:id', validarCargo, CargoController.atualizar)
+router.delete('cargos/:id', CargoController.excluir)
 
 // funcionarios
 
@@ -18,7 +22,11 @@ router.post('/cargos', validarCargo, CargoController.criar)
 
 
 // departamentos
-
+router.get('/departamentos', DepartamentoController.buscarTodos)
+router.get('/departamentos/:id', DepartamentoController.buscarPorID)
+router.post('/departamentos', validarDepartamento, DepartamentoController.criar)
+router.put('/departamentos/:id', validarDepartamento, DepartamentoController.atualizar)
+router.delete('/departamentos/:id', DepartamentoController.excluir)
 
 
 
